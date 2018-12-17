efdef058ca22a725e6fd19607a25e70dbf52ffcb
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-12-17 07:04:34 -0700

    Add files via upload

--- EVMetricsTables.js
+++ EVMetricsTables.js
@@ -1,22 +1,19 @@
 function loadData(cum_DataTable){
 
+
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 $.getJSON( "cum_EVMetrics.json", function(data) {
+  console.log(data);
+  loadData(data);
+});
+
+
+function loadData(cum_periodTable){
+
+}
+
+$.getJSON( "period_EVMetrics.json", function(data) {
   console.log(data);
   loadData(data);
 });
