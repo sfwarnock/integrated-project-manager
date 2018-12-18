75fdc18194242131ba0a01825e0b56c72eb1445d
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-12-18 07:20:51 -0700

    Add files via upload

--- EVMetricsTables.js
+++ EVMetricsTables.js
@@ -1,29 +1,4 @@
-var cum_DataTable = {
-    "Cumulative Earned Value": {
-        "18-Apr": 79800.0,
-        "18-Aug": 195400.0,
-        "18-Jul": 178000.0,
-        "18-Jun": 136600.0,
-        "18-Mar": 37900.0,
-        "18-May": 106200.0
-    },
-    "Cumulative Planned Value": {
-        "18-Apr": 86800.0,
-        "18-Aug": 190400.0,
-        "18-Jul": 165000.0,
-        "18-Jun": 143600.0,
-        "18-Mar": 43400.0,
-        "18-May": 122200.0
-    },
-    "Cumulative Total Cost": {
-        "18-Apr": 122250.0,
-        "18-Aug": 240600.0,
-        "18-Jul": 224600.0,
-        "18-Jun": 187100.0,
-        "18-Mar": 13500.0,
-        "18-May": 166600.0
-    }
-}
+var children = [{num: 6, name: 'me', phone: 7}, {num: 8, name: 'him', phone: 9}];
 
 function addHeaders(table, keys) {
   var row = table.insertRow();
@@ -36,7 +11,7 @@ function addHeaders(table, keys) {
 var table = document.createElement('table');
 for( var i = 0; i < children.length; i++ ) {
 
-  var child = cum_DataTable[i];
+  var child = children[i];
   if(i === 0 ) {
     addHeaders(table, Object.keys(child));
   }
@@ -48,4 +23,4 @@ for( var i = 0; i < children.length; i++ ) {
   })
 }
 
-document.getElementById('cum_DataTable').appendChild(table);
+document.getElementById('container').appendChild(table);
