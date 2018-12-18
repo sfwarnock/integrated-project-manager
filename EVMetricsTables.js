46afc2cd4cd38b3110dc9317d05250ad63edd130
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-12-18 06:14:21 -0700

    Add files via upload

--- EVMetricsTables.js
+++ EVMetricsTables.js
@@ -1,5 +1,14 @@
 function loadData(cum_DataTable){
+  var tableColumns = addAllColumnHeaders(cum_DataTable);
 
+  for (var i = 0; i < cum_DataTable.length, i++){
+    var row$ = $('<tr/>');
+    for (var colIndex = 0; colIndex < column.length; colIndex++){
+      var cellValue = cum_DataTable[i][columns[colIndex]];
+      if (cellValue == null) cellValue = "";
+      row$.append($('<td/>').html(cellValue));
+    }
+  }
 
 }
 
