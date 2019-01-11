fe4767b0e4f4714f45b52ee0cfcb2fb9eab3caee
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-01-11 07:02:02 -0700

    Add files via upload

--- project_reporting_graphToggle.js
+++ project_reporting_graphToggle.js
@@ -1,6 +1,8 @@
 function graphChange()
 {
-  var scurve = document.getElementById('projectSCurve');
-  projectSCurve.src = "s-curve.png";
-  document.body.appendChild(projectSCurve);
+  var scurve = document.getElementById('projectSCurve').value;
+  document.getElementById("scurve_graph").innerHTML = scurve;
+
+  var periodBar = document.getElementById('periodBar').value;
+  document.getElementById("period_graph").innerHTML = periodBar
 }
