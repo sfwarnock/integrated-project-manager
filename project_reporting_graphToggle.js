68440694d9d242ab85411e2327949a6f03a420dc
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-01-15 06:12:09 -0700

    Add files via upload

--- project_reporting_graphToggle.js
+++ project_reporting_graphToggle.js
@@ -1,14 +1,14 @@
 function graphChange()
 {
-  var scurve = document.getElementById('projectSCurve').value;
+  var scurve = document.getElementById('projectSCurve').src="s-curve.png";
   document.getElementById("scurve_graph").innerHTML = scurve;
 
-  var periodBar = document.getElementById('periodBar').value;
-  document.getElementById("period_graph").innerHTML = periodBar
+  var periodBar = document.getElementById('periodBar').src="bar-chart.png";
+  document.getElementById("period_graph").innerHTML = periodBar;
 
   var sideByside = document.getElementById('sideByside').value;
-  document.getElementById("sideByside_graph").innerHTML = periodBar
+  document.getElementById("sideByside_graph").innerHTML = sideByside;
 
   var dualAxis = document.getElementById('dualAxis').value;
-  document.getElementById("dualAxis_graph").innerHTML = periodBar
+  document.getElementById("dualAxis_graph").innerHTML = dualAxis;
 }
