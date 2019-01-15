334353df57019fb1cb2f5fa074acb585f9f0c861
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-01-15 06:53:07 -0700

    Add files via upload

--- project_reporting_graphToggle.js
+++ project_reporting_graphToggle.js
@@ -1,14 +1,5 @@
-function graphChange()
+function graphButtons()
 {
-  var scurve = document.getElementById('projectSCurve').src="s-curve.png";
-  document.getElementById("scurve_graph").innerHTML = scurve;
-
-  var periodBar = document.getElementById('periodBar').src="bar-chart.png";
-  document.getElementById("period_graph").innerHTML = periodBar;
-
-  var sideByside = document.getElementById('sideByside').value;
-  document.getElementById("sideByside_graph").innerHTML = sideByside;
-
-  var dualAxis = document.getElementById('dualAxis').value;
-  document.getElementById("dualAxis_graph").innerHTML = dualAxis;
+  document.getElementByID('s-curve').src='s-curve.png';
+  document.getElementByID('s-curve').style.display='block';
 }
