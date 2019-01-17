e21a710a76572d65b62865f085af44f4c25ff6d2
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-01-17 07:03:06 -0700

    Add files via upload

--- project_reporting_graphToggle.js
+++ project_reporting_graphToggle.js
@@ -1,3 +1,11 @@
+var index = 0;
+var imageList = ['s-curve.png', 'bar-chart.png'];
+
 function graphButtons(){
-  document.getElementByID("s-curve").innerHTML="<img src='s-curve.png' />";
+  index = index + 1;
+  if(index == imageList.length){
+    index = 0;
+  }
+  var s-curve = document.getElementById("s-curve");
+  s-curve.src = imageList[index];
 }
