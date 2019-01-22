6c19fefad9573a9917a71ba1422abbd7f3ecfddd
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-01-22 06:25:33 -0700

    Add files via upload

--- project_reporting_graphToggle.js
+++ project_reporting_graphToggle.js
@@ -2,4 +2,6 @@ function graphButtons(){
   document.getElementById('sCurve').src = 's-curve.png';
 
   document.getElementById('barChart').src = 'bar-chart.png';
+
+  document.getElementById('sideByside').src = ['s-curve.png', 'bar-chart.png'];
 }
