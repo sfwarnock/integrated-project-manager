cef840fe1c1abb9234e6d490a2c32f3a32e93edb
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-01-23 06:37:49 -0700

    Add files via upload

--- spikes/dynamicTable.js
+++ spikes/dynamicTable.js
@@ -1,31 +1,8 @@
-var tableData {
-    "ACWP": 240600.0,
-    "BAC": 571200.0,
-    "BCWP": 195400.0,
-    "BCWR": 375800.0,
-    "BCWS": 190400.0,
-    "CPI": 0.8121363258520365,
-    "CV": -45200.0,
-    "EAC_CPI": 703407.881773399,
-    "EAC_Composite": 691679.8067966851,
-    "EAC_General": 703448.2758620689,
-    "ETC": 462848.27586206887,
-    "PerACWP": 16000.0,
-    "PerBCWP": 25400.0,
-    "PerBCWS": 17400.0,
-    "PerCPI": 1.5875,
-    "PerCV": 9400.0,
-    "PerComp": 0.34208683473389356,
-    "PerPerComp": 1.0,
-    "PerSPI": 1.4597701149425288,
-    "PerSV": 8000.0,
-    "SPI": 1.0262605042016806,
-    "SV": 5000.0,
-    "TCPI_BAC": 0.8258156366387903,
-    "TCPI_EAC": 1.2340977157927677,
-    "VAC": -132248.27586206887
-}
-
-function loadData(tableData) {
-  
+function table() {
+  var table = document.getElementById("table");
+  var row = table.insertRow(0);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  cell1.innerHTML = "New Cell 1";
+  cell2.innerHTML = "New Cell 2";
 }
