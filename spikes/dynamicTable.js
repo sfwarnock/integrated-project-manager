c701581eed0ecf9d5495603abf1deeee33e1b355
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-01-23 06:57:33 -0700

    Add files via upload

--- spikes/dynamicTable.js
+++ spikes/dynamicTable.js
@@ -1,8 +1,15 @@
-function table() {
-  var table = document.getElementById("table");
-  var row = table.insertRow(0);
-  var cell1 = row.insertCell(0);
-  var cell2 = row.insertCell(1);
-  cell1.innerHTML = "New Cell 1";
-  cell2.innerHTML = "New Cell 2";
-}
+var datatable = {
+    "Cumulative Earned Value": {
+        "18-Apr": 79800.0,
+        "18-Aug": 195400.0,
+        "18-Jul": 178000.0,
+        "18-Jun": 136600.0,
+        "18-Mar": 37900.0,
+        "18-May": 106200.0
+};
+
+var tableCum = document.createElement("table");
+var tableCumRow = document.createElement("tableRow");
+  tableCum.appendChild(tableRowElement);
+
+var showTable = document.getElementById("showTable");
