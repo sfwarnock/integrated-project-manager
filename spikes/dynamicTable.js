4d6453d12a786ccbaf260a86a7ae1fea0737e6c0
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-01-24 06:47:48 -0700

    Add files via upload

--- spikes/dynamicTable.js
+++ spikes/dynamicTable.js
@@ -1,24 +1,61 @@
-var datatable = {
-    "Cumulative Earned Value": {
-        "18-Apr": 79800.0,
-        "18-Aug": 195400.0,
-        "18-Jul": 178000.0,
-        "18-Jun": 136600.0,
-        "18-Mar": 37900.0,
-        "18-May": 106200.0
-};
+var tableJSON = [
+ {
+   "Header": "Column1",
+   "Values": [
+     "75",
+     "79",
+     "83"
+   ]
+ },
+ {
+   "Header": "Column2",
+   "Values": [
+     "77",
+     "81",
+     "86",
+     "90"
+   ]
+ },
+ {
+   "Header": "Column3",
+   "Values": [
+     "98",
+     "117"
+   ]
+ }
+];
 
-var dataValues = dataObj["1"];
 
-for (var i dataValues) {
-  var table = document.getElementById("table");
-  var tableRow = document.createElement("tr");
-  var tableData = document.createElement("td");
+var tableDiv = document.createElement("table");
+var trElement =  document.createElement("tr");
+ tableDiv.appendChild( trElement );
 
-  for (var key in dataValues[i]){
-    var txt = document.createTextNode(key);
-    td.appendChild(txt);
-    tr.appendChild(td);
+var prDiv = document.getElementById("pr");
+tableJSON.forEach(function(a_column, index){
+if(a_column.Header)
+	{
+  var tdElement =  document.createElement("td");
+  tdElement.innerHTML = "<b>" + a_column.Header + "</b>";
+  trElement.appendChild( tdElement );
   }
-  table.appendChild(tr);
+  if(a_column.Values){
+  var allRows = tableDiv.childNodes;
+
+  for(var i=0 ;i< a_column.Values.length; i++)
+  {
+  var rowWanted = allRows[i+1];
+  if( !rowWanted )
+  {
+  	rowWanted = document.createElement("tr");
+   	tableDiv.appendChild( rowWanted );
+  }
+  if(rowWanted.childNodes.length==0)
+  	for(var j=0; j< tableJSON.length; j++){
+    rowWanted.appendChild( document.createElement("td") );
+    }
+
+  rowWanted.childNodes[ index ].innerHTML = a_column.Values[i];
+  }
 }
+});
+prDiv.appendChild(tableDiv);
