bbe0549a8beb5a1fa72eec779691cb0bd3e55f18
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-01-31 07:37:01 -0700

    Add files via upload

--- ui_allpages_layout.js
+++ ui_allpages_layout.js
@@ -1,10 +1,23 @@
-function dropdownlinks() {
-  document.getElementById("dropDownLink").classList.toggle("show");
+function projectInitiationlinks() {
+  document.getElementById("pIdropDownLink").classList.toggle("show");
 }
 
 window.onclick = function(e) {
   if (!e.target.matches('.dropButton')){
-  var myDropDown = document.getElementById("dropDownLink");
+  var myDropDown = document.getElementById("pIdropDownLink");
+    if (myDropDown.classList.contains('show')){
+      myDropDown.classList.remove('show');
+    }
+  }
+}
+
+function projectManagementlinks() {
+  document.getElementById("pMdropDownLink").classList.toggle("show");
+}
+
+window.onclick = function(e) {
+  if (!e.target.matches('.dropButton')){
+  var myDropDown = document.getElementById("pMdropDownLink");
     if (myDropDown.classList.contains('show')){
       myDropDown.classList.remove('show');
     }
